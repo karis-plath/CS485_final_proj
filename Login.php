58a8fb408a83e7638b4d0a8eca1154d0a25c9893
LindseyArndt <76603241+LindseyArndt@users.noreply.github.com>
2023-10-23 18:23:01 -0500

    added page

--- Login.php
+++ Login.php
@@ -1,51 +1,73 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="author" content="Karis and Lindsey">
-        <link rel="stylesheet" href="Style_sheet.css">
-        <title>Login</title>
-    </head>
-    
-    <h1>Welcome</h1>
+<head>
+<meta charset="UTF-8">
+<meta name="author" content="Karis and Lindsey">
+<link rel="stylesheet" href="Style_sheet.css">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<title>Login</title>
+</head>
+<div class="d-flex justify-content-center container">
 
-    <form action="test_login.php" method="POST">
-    	<input id ="inputSize" type="submit" name="submitLogout" value="Logout">
-    </form>
-    
+<h1>Login</h1>
+	<form method=POST>
+		<div class="input-group">
+			<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+			<input id="user" type="text" class="form-control well-sm input-lg" name="email"
+				placeholder="Username">
+		</div>
+		<div class="input-group">
+			<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+			<input id="pass" type="text" class="form-control well-sm input-lg"
+				name="password" placeholder="Password">
+		</div>
+		 <button name="sub" type="submit" class="btn btn-default">Submit</button>
+	</form>
+</div>
     <?php
     if (isset($_POST['sub'])) {
-    
         $servername = "localhost";
         $username = "root"; // user name
-        $password = "1111"; // password used to login MySQL server
+        $password = "111111"; // password used to login MySQL server
         $dbname = "cs485_labs";
-    
+
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-    
+
         // Check connection
         if ($conn->connect_error) {
-    
             die("Connection failed: " . $conn->connect_error);
         }
-    
+
         // Use prepared statement in SQL
-        $stmt = $conn->prepare("INSERT INTO login (username, password) VALUES (?, ?)");
-    
         $name = $_POST['user'];
         $pass = $_POST['pass'];
-    
-        $stmt->bind_param("ss", $name, $pass); // ss lists types of parameters, they two strings
-        $stmt->execute();
-    
-        echo "New records created successfully";
-    
-        $stmt->close();
+        $sql = "SELECT * FROM login WHERE username = '$name' AND password = '$pass'";
+
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) { // info about query"https://www.w3schools.com/php/php_mysql_select.asp
+                if ($row["username"] == $name) {
+                    $check = 1;
+                    if ($row["password"] != $pass) {
+                        echo '<script>alert("Username does not match password")</script>';
+                    } else {
+                        session_start();
+                        $_SESSION["username"] = $name;
+                        header("Location: search_page.php");
+                        exit();
+                    }
+                }
+            }
+            if ($check == 0) {
+                echo '<script>alert("Username does not exist")</script>';
+            }
+        }
+        $result->close();
         $conn->close();
     }
-    
-    ?>
-
+    ?> 
 </html>
---
+++ search_page.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+</head>
+<body>
+<h1>You made it</h1>
+</body>
+<?php
+?>
+</html>
